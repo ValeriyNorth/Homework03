2778abffc197551ccd6557524f88bcec2b869863
ValeriyNorth <66730404+ValeriyNorth@users.noreply.github.com>
2020-07-03 13:08:51 +0300

    Add files via upload

---
+++ StepByStep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Homework_03
+{
+    public class StepByStep
+    {
+        public StepByStep()
+        { }
+        public string LogUser { get; set; } // Логин пользователя
+        public int NumGame { get; set; }   // Номер в игре
+    }
+}
