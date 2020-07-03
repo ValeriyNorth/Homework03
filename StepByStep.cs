c807c3431d68a38c9347b78d651def8eb753addc
ValeriyNorth <66730404+ValeriyNorth@users.noreply.github.com>
2020-07-03 14:01:28 +0300

    Add files via upload

--- StepByStep.cs
+++ StepByStep.cs
@@ -7,9 +7,26 @@ namespace Homework_03
 {
     public class StepByStep
     {
+        public List<int> trueNum = new List<int> { 1, 2, 3, 4};  // Задаем список допустимых значений
+
         public StepByStep()
         { }
         public string LogUser { get; set; } // Логин пользователя
         public int NumGame { get; set; }   // Номер в игре
+
+        /// <summary>
+        /// Функция поиска допустимых значений в списке
+        /// </summary>
+        /// <param name=""></param>
+        public FList(int FNum)
+        {
+            int k = 0;
+            foreach (var tn in this.trueNum)
+            {
+                if (FNum == tn.ElementAt(k++))
+                { }
+
+            }
+        }
     }
 }
